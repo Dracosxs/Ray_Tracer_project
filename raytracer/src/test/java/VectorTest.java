e35f13b60c7d44c303f2cf66be71763dd9a8fd32
Tomm <tom.ketzinger6@etu.univ-lorraine.fr>
2025-11-12 11:08:22 +0100

    ajout pom.xml + test vector

---
+++ raytracer/src/test/java/VectorTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VectorTest {
+
+    private static final double EPSILON = 1e-9;
+
+    @Test
+    public void testAddition() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(4, -2, 0);
+        Vector res = (Vector) v1.add(v2);
+        assertEquals(5, res.getX(), EPSILON);
+        assertEquals(0, res.getY(), EPSILON);
+        assertEquals(3, res.getZ(), EPSILON);
+    }
+
+    @Test
+    public void testSubtraction() {
+        Vector v1 = new Vector(3, 5, 7);
+        Vector v2 = new Vector(1, 2, 3);
+        Vector res = (Vector) v1.sub(v2);
+        assertEquals(2, res.getX(), EPSILON);
+        assertEquals(3, res.getY(), EPSILON);
+        assertEquals(4, res.getZ(), EPSILON);
+    }
+
+    @Test
+    public void testDotProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(4, 5, 6);
+        assertEquals(32, v1.dot(v2), EPSILON);
+    }
+
+    @Test
+    public void testCrossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(4, 5, 6);
+        Vector cross = v1.cross(v2);
+        assertEquals(-3, cross.getX(), EPSILON);
+        assertEquals(6, cross.getY(), EPSILON);
+        assertEquals(-3, cross.getZ(), EPSILON);
+    }
+
+    @Test
+    public void testNormalize() {
+        Vector v = new Vector(3, 0, 4);
+        Vector norm = (Vector) v.normalize();
+        assertEquals(1.0, norm.length(), EPSILON);
+    }
+
+    @Test
+    public void testIsZero() {
+        Vector v = new Vector(1e-10, 0, 0);
+        assertTrue(v.isZero());
+    }
+}
